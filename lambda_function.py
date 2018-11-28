921596380f431311f7f3528a4ba9e850462f8cb3
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-11-28 16:36:48 +0900

    Refactor lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,20 @@
 import boto3
 
 
-def lambda_handler(event, context):
-    print(event)
+def put_event():
     events = boto3.client('events')
-    result = events.put_events(Entries=[
+    return events.put_events(Entries=[
         {
             'Source': 'git-svn-sync-lambda',
             'DetailType': '{}',
             'Detail': '{}'
         }
     ])
+
+
+def lambda_handler(event, context):
+    print(event)
+    result = put_event()
     print(result)
     # Format to be consumed by API Gateway proxy:
     # https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-output-format
